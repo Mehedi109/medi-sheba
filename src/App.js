6b104f119ee906d1f7cbc8ec6434ad4c263693cf
Mehedi <mehedialam109@gmail.com>
2021-10-18 12:16:28 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+      {/* slider 1 : [url=https://ibb.co/hmxJwbk][img]https://i.ibb.co/vjC6Rpb/photo-1532938911079-1b06ac7ceec7-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-MXxhb.jpg[/img][/url]
+
+      slider 2: [url=https://ibb.co/0ZRVwSy][img]https://i.ibb.co/FJLqPM4/photo-1504813184591-01572f98c85f-ixlib-rb-1-2.jpg[/img][/url]
+
+      slider 3 : [url=https://ibb.co/PwRgjdn][img]https://i.ibb.co/qkzpjvf/photo-1514416205405-075ab2f15964-ixlib-rb-1-2.jpg[/img][/url]
+
+      slider 4 : [url=https://ibb.co/HNXXCN4][img]https://i.ibb.co/GsFF9sp/photo-1578496781985-452d4a934d50-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwx-Mj-A3f-DB8-MXxhb.jpg[/img][/url]
+
+      slider 5 : [url=https://ibb.co/rwLmR2h][img]https://i.ibb.co/vjrH64W/photo-1612277795421-9bc7706a4a34-ixlib-rb-1-2.jpg[/img][/url]
+      </header> */}
     </div>
   );
 }
